9e438dc1f1a8413ba0f74abc037f2f91bbc44dac
nanito <nanito@nanito-VirtualBox>
2015-11-19 17:01:49 -0500

    comenzando a construir la coneccion con la base de datos

--- src/main/java/model_spring_rest/DAO.java
+++ src/main/java/model_spring_rest/DAO.java
@@ -5,20 +5,27 @@
  */
 package model_spring_rest;
 import com.mycompany.proyecto_tienda.*;
+import com.sun.jndi.ldap.Connection;
+import java.sql.Driver;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  *
  * @author nanito
  */
-@Configuration
 public class DAO {
-    
-    public DAO(){
+    @Autowired
+    JdbcTemplate jdbc;
+    //Class.forName("org.postgresql.Driver");
+    String url = "jdbc:postgresql://aretico.com:5432/software2";
+    Connection connection = null;
+    //connection = DriverManager.getConnection(url, this.usuario, this.pwd);
+    public DAO(){ 
         
     }
     
